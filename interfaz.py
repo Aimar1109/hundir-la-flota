f295110ddf1f6ae40350f92ae88e04d938e0f1ce
aimar <aimar.junagore@gmail.com>
2024-02-09 20:49:48 +0100

    Beta Mapa barcos hecha y interfaz con error de bucle infinito

---
+++ interfaz.py
@@ -0,0 +1,289 @@
+import tkinter
+import time
+import mapas_barcos
+from random import randint, choice
+from functools import partial
+from tkinter import messagebox
+
+barcos = [5, 4, 3, 2, 2, 1, 1]
+pb = 7
+player = mapas_barcos.Mapa('tu')
+bot = mapas_barcos.MapaBot()
+bot.colocar_flota_aleatoria()
+d_i = []
+d_l = []
+u_d = []
+posibles_barcos = []
+posibles_barco = 0
+b = False
+e = False
+ori = None
+posibles = []
+p_ud = None
+
+# main window of the game
+
+window = tkinter.Tk()
+window.geometry("1165x565+10+10")
+window.title("Hundir La Flota")
+
+def start():
+    bot = mapas_barcos.MapaBot()
+    bot.colocar_flota_aleatoria()
+    return bot
+
+def reset():
+    pass
+
+def end():
+    pass
+
+def variables(mapa):
+    pass
+    
+
+def game(event):
+    global player, x, y, barcos, canvas, bot, pb
+    x = event.x // 50
+    y = event.y // 50
+    if pb > 0 and x < 10:
+        window.bind("<Right>", put_barco)
+        window.bind("<Left>", put_barco)
+        window.bind("<Up>", put_barco)
+        window.bind("<Down>", put_barco)
+        pb -= 1
+    elif pb == 0:
+        if x >= 11 and x < 21:
+            int_disparo()
+            bot_disparo()
+        if len(player.barcos) < 1:
+            messagebox.showwarning("Advertencia", "Perdiste!")
+        elif len(bot.barcos) < 1:
+            messagebox.showwarning("Advertencia", "Ganaste!")
+
+
+
+def put_barco(event):
+    global player, x, y, barcos, canvas, bot
+    if event.keysym == 'Right':
+        barco = mapas_barcos.Barco(get_key_by_value(player.tipos_barco, barcos[0]), barcos[0], [y, x], False, 'd')
+    elif event.keysym == 'Left':
+        barco = mapas_barcos.Barco(get_key_by_value(player.tipos_barco, barcos[0]), barcos[0], [y, x], False, 'i')
+    elif event.keysym == 'Up':
+        barco = mapas_barcos.Barco(get_key_by_value(player.tipos_barco, barcos[0]), barcos[0], [y, x], True, 'ar')
+    elif event.keysym == 'Down':
+        barco = mapas_barcos.Barco(get_key_by_value(player.tipos_barco, barcos[0]), barcos[0], [y, x], True, 'ab')
+    barco.crear_barco()
+    if player.validar_barco(barco):
+        player.barcos.append(barco)
+        barcos.remove(barcos[0])
+        player.colocar_barcos()
+        for barco in player.barcos:
+            for posicion in barco.posiciones:
+                canvas.create_rectangle(5+(50*posicion[1]),
+                                        5+(50*posicion[0]), 
+                                        5+(50*(posicion[1]+1)), 
+                                        5+(50*(posicion[0]+1)), 
+                                        fill='grey')
+    else:
+        messagebox.showwarning("Advertencia", "No puedes  colocar más de un barco en una misma casilla.")
+
+
+def get_key_by_value(dictionary, value):
+    for key, val in dictionary.items():
+        if val == value:
+            return key
+    return None
+
+def int_disparo():
+    global d_i
+    if [x, y] not in d_i:
+        d, e = player.disparo(bot, x-11, y)
+        if d:
+            canvas.create_rectangle(5+(50*x), 
+                                    5+(50*y), 
+                                    5+(50*(x+1)),
+                                    5+(50*(y+1)), 
+                                    fill='red')
+            if e == 2:
+                messagebox.showwarning("Advertencia", "Hundido!")
+
+        else:
+            canvas.create_text(30+(50*x),
+                               30+(50*y),
+                               text='X',
+                               fill="black",
+                               font='tkDefaeultFont 42')
+        d_i.append([x, y])
+
+
+def bot_disparo():
+    global b, e, ori, u_d, posibles
+    y_b, x_b, ori = bot_disparo_logica(b, e, ori)
+    b, e = bot.disparo(player, y_b, x_b)
+    if b:
+        canvas.create_rectangle(5+(50*x_b), 
+                                5+(50*y_b), 
+                                5+(50*(x_b+1)), 
+                                5+(50*(y_b+1)), 
+                                fill='red')
+        if e == 2:
+                messagebox.showwarning("Advertencia", "Hundido!")
+                u_d = []
+                posibles = []
+        elif e == 1:
+            messagebox.showwarning("Advertencia", "Tocado!")
+            u_d.append([y_b, x_b])
+    else:
+        canvas.create_text(30+(50*x_b),
+                           30+(50*y_b),
+                           text='X',
+                           fill="black",
+                           font='tkDefaeultFont 42')
+        try:
+            posibles.remove([y_b, x_b])
+        except:
+            pass
+
+def bot_disparo_logica(b, e, ori):
+    global u_d, posibles_barcos, posibles_barco, posibles, bot, d_l, p_ud
+    a = 0
+    while True:
+        if len(u_d) == 0:
+            y_b = randint(0,9)
+            x_b = randint(0,9)
+        elif len(u_d) == 1:
+            if len(posibles) == 0:
+                p_ud = u_d[0]
+                posibles = [[u_d[0][0]+1, u_d[0][1]], 
+                            [u_d[0][0]-1, u_d[0][1]],
+                            [u_d[0][0], u_d[0][1]+1],
+                            [u_d[0][0], u_d[0][1]-1]]
+                for pos in posibles:
+                    if pos[0] < 0 or pos[0] > 9 or pos[1] < 0 or pos[1] > 9 or pos in bot.disparos:
+                        posibles.remove(pos)
+            d = choice(posibles)
+            if d[0] == p_ud[0]+1:
+                ori = 'd'
+            elif d[0] == p_ud[0]-1:
+                ori = 'u'
+            elif d[1] == p_ud[1]+1:
+                ori = 'r'
+            elif d[1] == p_ud[1]-1:
+                ori = 'l'
+            y_b = d[0]
+            x_b = d[1]
+        else:
+            y_b = u_d[-1][0]
+            x_b = u_d[-1][1]
+            print('\n', y_b, x_b, '\n')
+            if b  and e == 1:
+                if ori == 'd':
+                    y_b += 1
+                elif ori == 'u':
+                    y_b -= 1
+                elif ori == 'r':
+                    x_b += 1
+                elif ori == 'l':
+                    x_b -= 1
+        if x_b > 9:
+            x_b = p_ud[1] -1
+            ori = 'l'
+        elif x_b < 0:
+            x_b = p_ud[1] +1
+            ori = 'r'
+        elif y_b > 9:
+            y_b = p_ud[0] -1
+        elif  y_b < 0:
+            y_b = p_ud[0] +1
+            ori = 'd'
+
+        
+        print('\n', y_b, x_b)
+        print(bot.disparos)
+        print(u_d)
+        print(posibles)
+        print(p_ud)
+        if [y_b, x_b] not in bot.disparos:
+            break
+        elif  len(u_d) <= 2:
+            if ori == 'r':
+                x_b = p_ud[1] -1
+                ori = 'l'
+            elif ori == 'l':
+                x_b = p_ud[1] +1
+                ori = 'r'
+            elif ori == 'u':
+                x_b = p_ud[0] -1
+                ori = 'd'
+            elif  ori == 'd':
+                x_b = p_ud[0] +1
+                ori = 'u'
+            break
+    return y_b, x_b, ori
+    
+                
+
+
+canvas = tkinter.Canvas(window,
+                        height = 510,
+                        width = 1060,
+                        background = "blue")
+canvas.place(x = 100, y = 50)
+
+canvas.create_line(2.5,5,1105,2.5,fill = "black",
+                       width = 5)
+
+canvas.create_line(5,5,5,510,fill = "black",
+                       width = 5)
+
+canvas.create_line(1058,5,1058,510,fill = "black",
+                       width = 5)
+
+canvas.create_line(5,508,1105,508,fill = "black",
+                       width = 5)
+
+canvas.create_line(530,0,530,510,fill = "black",
+                       width = 50)
+
+for i in range(5,1150,50):
+    canvas.create_line(i,5,i,610,fill = "black",
+                       width = 2)
+
+for x in range(5,600,50):
+    canvas.create_line(5,x,1160,x,fill = "black",
+                       width = 2)
+
+button_01 = tkinter.Button(window, text = "START",
+                           command = start)
+
+button_01.place(x = 15, y = 55, height = 30, width = 65 )
+
+button_02 = tkinter.Button(window, text = "RESET",
+                           command = reset)
+button_02.place(x = 15, y = 105, height = 30, width = 65 )
+
+button_03 = tkinter. Button(window, text = "END",
+                             command = end)
+
+button_03.place(x = 15, y = 155, height = 30, width = 65)
+
+canvas_n = tkinter.Canvas(window,
+                        height = 50,
+                        width = 1060,
+                        background = None)
+canvas_n.place(x = 100, y = 0)
+
+canvas_n.create_text((250, 25),
+                    text='TÚ',
+                    fill="black",
+                    font='tkDefaeultFont 42')
+
+canvas_n.create_text((800, 25),
+                    text='BOT',
+                    fill="black",
+                    font='tkDefaeultFont 42')
+
+canvas.bind("<Button-1>", game)
+
+window.mainloop()
---
+++ mapas_barcos.py
@@ -0,0 +1,228 @@
+# HUNDIR LA FLOTA
+
+import os
+import random
+import time
+
+class Mapa():
+    def __init__(self, name):
+        self.name = name
+        self.mar = 0
+        self.mapa = [[self.mar]*10 for _ in range(10)]
+        self.barcos = []
+        self.disparos = []
+        self.tipos_barco = {"Portaaviones": 5,
+                       "Acorazado": 4, 
+                       "Crucero": 3, 
+                       "Destructor": 2,
+                       "Submarino": 1}
+
+    def actualizar_mapa(self, fila, columna, valor):
+        self.mapa[fila][columna] = valor
+    
+    def colocar_barcos(self):
+        for barco in self.barcos:
+            for posicion in barco.posiciones:
+                self.actualizar_mapa(posicion[0], posicion[1], barco.tamaño)
+
+    def crear_barco(self, name, tamaño, fila, columna, orientacion, direccion):
+        nuevo_barco = Barco(name, tamaño, [fila, columna], orientacion, direccion)
+        self.barcos.append(nuevo_barco)
+
+    def colocar_flota(self):
+        for barco, tamaño in self.tipos_barco.items():
+            if barco == "Destructor" or barco == "Submarino":
+                cantidad = 2
+            else:
+                cantidad = 1
+            for i in range(cantidad):
+                while True:
+                    self.display_un_mapa()
+                    print(f"\nColocar {barco} {i+1} de tamaño {tamaño}:")
+                    fila = int(input("Ingrese la fila inicial: "))
+                    columna = int(input("Ingrese la columna inicial: "))
+                    if tamaño == 1:
+                        orientacion = None
+                        direccion = None
+                    else:
+                        orientacion = input("Ingrese la orientación (vertical)/(horizontal): ")
+                        if orientacion == 'vertical':
+                            orientacion = True
+                            direccion = input("Ingrese la orientación Vertical: (ar)/(ab): ")
+                        else:
+                            orientacion = False
+                            direccion = input("Ingrese la orientación Horizontal (d)/(i): ")
+                    nuevo_barco = Barco(barco, tamaño, [fila, columna], orientacion, direccion)
+                    nuevo_barco.crear_barco()
+                    if self.validar_barco(nuevo_barco):
+                        break
+                    else:
+                        os.system('cls')
+                        print('\n       NO ES POSIBLE PONER EL BARCO AHI')
+                        time.sleep(3)
+                self.barcos.append(nuevo_barco)
+                self.colocar_barcos()
+    
+    def display_un_mapa(self):
+        os.system('cls')
+        print(f"\t       {self.name.upper()}")
+        for i in range(11):
+            if i == 0:
+                for x in range(10):
+                    if x == 0:
+                        print(f'   {x}', end="")
+                    elif x == 9:
+                        print(f'  {x}')
+                    else:
+                        print(f'  {x}', end="")
+            else:
+                print(f'{i-1} ' + str(self.mapa[i-1]))
+
+    def display_juego(self, bot):
+        os.system('cls')
+        print(f"\t       {self.name.upper()}\t\t\t\t    BOT")
+        for i in range(11):
+            if i == 0:
+                for x in range(10):
+                    if x == 0:
+                        print(f'   {x}', end="")
+                    else:
+                        print(f'  {x}', end="")
+                print('   |  ', end='')
+                for x in range(10):
+                    if x == 0:
+                        print(f'   {x}', end="")
+                    elif x == 9:
+                        print(f'  {x}')
+                    else:
+                        print(f'  {x}', end="")
+            else:
+                print(f"{i-1} {self.mapa[i-1]}  | {i-1}  {bot.mapa[i-1]}")
+
+
+    
+    def validar_barco(self, n_barco):
+        for pos in n_barco.posiciones:
+            if pos[0] < 0 or pos[1] < 0 or pos[0] > 9 or pos[1] > 9:
+                return False
+            for barco in self.barcos:
+                if pos in barco.posiciones:
+                    return False
+        return True
+    
+    def disparo(self, contrario, y, x):
+        col = False
+        self.disparos.append([y, x])
+        for barco in contrario.barcos:
+            if [y, x] in barco.posiciones:
+                col = True
+                contrario.actualizar_mapa(y, x, '-')
+                barco.posiciones.remove([y, x])
+                if len(barco.posiciones) != 0:
+                    return True, 1
+                else:
+                    contrario.barcos.remove(barco)
+                    return True, 2
+        if not col:
+            contrario.actualizar_mapa(y, x, 'X')
+            return False, 0
+            
+
+
+class MapaBot(Mapa):
+    def __init__(self):
+        self.name = 'Bot'
+        self.mar = 0
+        self.mapa = [[self.mar]*10 for _ in range(10)]
+        self.barcos = []
+        self.disparos = []
+
+    def colocar_flota_aleatoria(self):
+        tipos_barco = {"Portaaviones": 5,
+                       "Acorazado": 4,
+                       "Crucero": 3,
+                       "Destructor": 2,
+                       "Submarino": 1}
+
+        for barco, tamaño in tipos_barco.items():
+            if barco == "Destructor" or barco == "Submarino":
+                cantidad = 2
+            else:
+                cantidad = 1
+            for i in range(cantidad):
+                orientacion = random.choice(['vertical', 'horizontal'])
+                if orientacion == 'vertical':
+                    orientacion = True
+                    direccion = random.choice(['ar', 'ab'])
+                else:
+                    orientacion = False
+                    direccion = random.choice(['d', 'i'])
+                while True:
+                    fila = random.randint(0, 9)
+                    columna = random.randint(0, 9)
+                    nuevo_barco = Barco(barco, tamaño, [fila, columna], orientacion, direccion)
+                    nuevo_barco.crear_barco()
+                    if self.validar_barco(nuevo_barco):
+                        break
+                self.barcos.append(nuevo_barco)
+    
+
+class Barco():
+    def __init__(self, nombre, tamaño, p_posicion, vertical, direccion):
+        self.nombre = nombre
+        self.tamaño = tamaño
+        self.p_posicion = p_posicion
+        self.vertical = vertical
+        self.direccion = direccion
+        self.posiciones = []
+
+    def crear_barco(self):
+        if self.tamaño > 1:
+            for i in range(self.tamaño):
+                if self.vertical:
+                    if self.direccion == 'ab':
+                        self.posiciones.append([self.p_posicion[0]+i, self.p_posicion[1]])
+                    else:
+                        self.posiciones.append([self.p_posicion[0]-i, self.p_posicion[1]])
+                else:
+                    if self.direccion == 'd':
+                        self.posiciones.append([self.p_posicion[0], self.p_posicion[1]+i])
+                    else:
+                        self.posiciones.append([self.p_posicion[0], self.p_posicion[1]-i])
+        else:
+            self.posiciones.append([self.p_posicion[0], self.p_posicion[1]])
+    
+
+#game = True
+#
+#user_mapa = Mapa(input('Tu nombre de usuario: '))
+#user_mapa.colocar_flota()
+#
+#bot_mapa = MapaBot()
+#bot_mapa.colocar_flota_aleatoria()
+#
+#while game:
+#    user_mapa.display_juego(bot_mapa)
+#    print(f'\nTurno de {user_mapa.name}')
+#    while True:
+#        y_u = int(input('Fila: '))
+#        x_u = int(input('Columna: '))
+#        if [y_u, x_u] not in user_mapa.disparos:
+#            break
+#    user_mapa.disparo(bot_mapa, y_u, x_u)
+#    user_mapa.display_juego(bot_mapa)
+#    print(f'\nTurno de {bot_mapa.name}')
+#    while True:
+#        y_b = random.randint(0,9)
+#        x_b = random.randint(0,9)
+#        if [y_b, x_b] not in bot_mapa.disparos:
+#            break
+#    bot_mapa.disparo(user_mapa, y_b, x_b)
+#    if len(user_mapa.barcos) == 0:
+#        user_mapa.display_juego(bot_mapa)
+#        print("\n\t\t\tHAS PERDIDO!!!")
+#        game = False
+#    elif len(bot_mapa.barcos) == 0:
+#        user_mapa.display_juego(bot_mapa)
+#        print("\n\t\t\tHAS PERDIDO!!!")
+#        game = False
